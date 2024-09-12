440634abcdc52114066a9a5680b43a895bbe6e2c
MORPHS01 <ayonimofe2202@gmail.com>
2024-09-12 12:28:48 +0100

    responsiveness

--- components/sections/Subscribe.tsx
+++ components/sections/Subscribe.tsx
@@ -5,7 +5,7 @@ function Subscribe() {
     <section id="contact" className="w-[80%] mx-auto py-[3.5em] flex flex-col justify-center items-center">
       <p className="text-sm text-white tracking-widest mb-6">35,000+ ALREADY JOINED</p>
       <p className="font-semibold text-2xl leading-[3rem] text-white mb-5 text-center">Stay up-to-date with what we’re doing</p>
-      <div className="flex gap-6 justify-center">
+      <div className="flex gap-6 justify-center max-sm:flex-col">
         <input required type="email" placeholder="Enter your email address" className="rounded-md px-5 text-sm focus:outline-none"/>
         <Button label="Contact Us" textColor="text-white" backgroundColour="bg-softRed" borderColor="border-softRed" hoverTextColor="hover:text-softRed" hoverBorderColor="hover:border-softRed"/>
       </div>
